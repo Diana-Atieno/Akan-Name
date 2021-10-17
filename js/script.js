3d6b66cb56fa60853fc89eef67203ed52fd710f4
Diana <kopiyodiana@gmail.com>
2021-10-17 16:23:57 +0300

    added a get date input on js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// get date input //
+let year = document.getElementById("year").value 
+console.log(year);
+
+// get gender input //
+
+//applying the provided formula //
+
+// convert Akan Names into arrays//
+// male //
+// female //

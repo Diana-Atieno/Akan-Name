26e427e7d3651ee124cd8a1970bd54a68f160667
Diana <kopiyodiana@gmail.com>
2021-10-17 20:00:27 +0300

    declared variables on the script.js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// declaring variables //
+var CC, YY, MM, DD, d, dayValue;
+var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+
 // get date input //
 let year = document.getElementById("year").value 
 console.log(year);

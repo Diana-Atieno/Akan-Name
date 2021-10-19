ebc73b422b2acab8be7443658eb2929c5e038df5
Diana <kopiyodiana@gmail.com>
2021-10-19 08:55:20 +0300

    write formula for DayOfWeek in js

--- js/script.js
+++ js/script.js
@@ -4,11 +4,18 @@ formName.addEventListener("submit", (e) => {
     e.preventDefault();
 // getting gender by Id
 gender = document.getElementById("gender").value
-dateOfBirth = document.getElementById("year").value
-dateOfBirth = new Date(dateOfBirth);
+DayOfMonth = document.getElementById("year").value
+DayOfMonth = new Date(DayOfMonth);
 
 //Declaring variables and changing yearOfBirth to string
-CC = yearOfBirth.toString().substr(0, 2);
+CC = centuryYear.toString().substr(0, 2);
 YY = yearOfBirth.toString().substr(2, 2);
-MM = birthMonth
-DD = dayOfBirth
+MM = monthBorn
+DD = DayOfMonth
+
+let yearOfBirth = DayOfMonth.getFullYear();
+let monthBorn = DayOfMonth.getMonth() + 1;
+let DayOfMonth = DayOfMonth.getDate();
+
+//formula for DayOfWeek
+dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;

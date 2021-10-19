27ee546bf6b0b59578d20c471bf5228f0c166eeb
Diana <kopiyodiana@gmail.com>
2021-10-19 08:48:01 +0300

    declaring variables

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ formName.addEventListener("submit", (e) => {
 // getting gender by Id
 gender = document.getElementById("gender").value
 dateOfBirth = document.getElementById("year").value
+dateOfBirth = new Date(dateOfBirth);
+
+//Declaring variables and changing yearOfBirth to string
+CC = yearOfBirth.toString().substr(0, 2);
+YY = yearOfBirth.toString().substr(2, 2);
+MM = birthMonth
+DD = dayOfBirth

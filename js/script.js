1f29fb4b0dd07cb1c4977b74126b2d7b4863351c
Diana <kopiyodiana@gmail.com>
2021-10-19 12:03:36 +0300

    declaring bornDay and initializing string variables for day

--- js/script.js
+++ js/script.js
@@ -3,25 +3,34 @@ let formName = document.getElementById("formName");
 formName.addEventListener("submit", (e) => {
     e.preventDefault();
 // getting gender by Id
-gender = document.getElementById("gender").value
-DayOfMonth = document.getElementById("year").value
-DayOfMonth = new Date(DayOfMonth);
+    gender = document.getElementById("gender").value
+    dateOfBirth = document.getElementById("year").value
+
+    dateOfBirth = new Date(dateOfBirth);
 
 //Declaring variables and changing yearOfBirth to string
-CC = centuryYear.toString().substr(0, 2);
-YY = yearOfBirth.toString().substr(2, 2);
-MM = monthBorn
-DD = DayOfMonth
+let yearOfBirth = dateOfBirth.getFullYear();
+let birthMonth = dateOfBirth.getMonth() + 1;
+let dayOfBirth = dateOfBirth.getDate();
 
-let yearOfBirth = DayOfMonth.getFullYear();
-let monthBorn = DayOfMonth.getMonth() + 1;
-let DayOfMonth = DayOfMonth.getDate();
+
+CC = yearOfBirth.toString().substr(0, 2);
+YY = yearOfBirth.toString().substr(2, 2);
+MM = birthMonth
+DD = dayOfBirth
+
+
 
 //formula for DayOfWeek
 dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
 
 // converting the day of the week to integer
 dayOfTheWeek = parseInt(dayOfTheWeek);
+//declaring bornDay and initializing string variables for day
+let bornDay;
+
+femaleAkanNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+maleAkanNames =  ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
 
 //declaring akan names and initializing string variables of names
 
@@ -30,11 +39,8 @@ let akanName;
     femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-//declaring bornDay and initializing string variables for day
-let bornDay;
 
-    femaleAkanNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    maleAkanNames =  ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 //adding control flows 
 if (dayOfTheWeek == 0 && gender == "female") {
     bornDay = "Sunday";
@@ -91,6 +97,8 @@ if (dayOfTheWeek == 0 && gender == "female") {
     bornDay = "Saturday";
     akanName = maleAkanNames[6];
 }
-
+//getting the result function
+let result = document.getElementById("get-result").innerHTML = "The Day you were Born is " + bornDay + " and therefore, your Akan Name is " + akanName + "!"
+    document.getElementById("formName").reset();
 
 } );
